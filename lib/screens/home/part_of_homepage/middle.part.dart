d5b3f83b4324c61f12a8c0982c9fdc6db032af06
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-07-21 07:09:14 +0545

    Little UI Changes

--- lib/screens/home/part_of_homepage/middle.part.dart
+++ lib/screens/home/part_of_homepage/middle.part.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class MidddlePart extends StatelessWidget {
   const MidddlePart({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 12.0,right: 5 ),
+      padding: const EdgeInsets.only(top: 12.0, right: 10),
       child: Column(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Top Related Freelances",style: GoogleFonts.roboto(
-                fontWeight: FontWeight.bold,
-                fontSize: 20
-              ),),
-
-              const Text("View All",style: TextStyle(decoration: TextDecoration.underline,
-              color: Colors.blue),)
-      
+              Text(
+                "Top Related",
+                style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              const Text(
+                "See More",
+                style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    color: Color.fromARGB(193, 58, 58, 58)),
+              )
             ],
           )
         ],
